76c703ca7cf356148b0da7bbc450eb11ce400644
reza khaleghifar <khalghi75@gmail.com>
2021-04-11 22:59:21 -0700

    start project

---
+++ app/src/main/java/ir/ac/tvu/jabarian/MainActivity.kt
@@ -0,0 +1,11 @@
+package ir.ac.tvu.jabarian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
